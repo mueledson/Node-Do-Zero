5396ba185567c1d9f7c2d66abbdb378be56bfaae
edson@app100.com.br <mueledson>
2023-09-07 11:37:41 -0300

    CRUD Completo Pronto

--- server.js
+++ server.js
@@ -22,8 +22,6 @@ const database = new DatabaseMemory()
 server.post('/videos', (request, reply) => {
     const {title, description, duration} = request.body
 
-    console.log(body)
-
     database.create({
         title,
         description,
@@ -33,20 +31,36 @@ server.post('/videos', (request, reply) => {
     return reply.status(201).send()
 })
 
-server.get('/videos', () => {
-    return 'Node!'
+server.get('/videos', (request) => {
+    const search = request.query.search
+
+    const videos = database.list(search)
+
+    return videos
 })
 
-server.put('/videos/:id', () => {
-    return 'Hello!'
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    const {title, description, duration} = request.body
+
+    database.update(videoId, {
+        title,
+        description,
+        duration
+    })
+
+    return reply.status(204).send
 })
 
-server.delete('/videos/:id', () => {
-    return 'Hello!'
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send
 })
 
 server.listen({
     port: 3333
 })
-
 // Fastify é um microFramework
